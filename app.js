18121372c9f96fcd26788eb841ffb148bffd59e7
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-11-21 17:33:44 +0100

    added pokemon db file

--- app.js
+++ app.js
@@ -1,44 +1,22 @@
 const express = require('express');
+let pokemons = require('./pokemons');
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!!"));
 
-app.get("/api/pokemon/:id", (req, res) => {
-    const id = req.params.id;
+app.get("/api/pokemons/", (req, res) => {
+    res.status(200).json({response: `Il y a ${pokemons.length} pokemons dans le pokédex !`});
+})
+app.get("/api/pokemons/:id", (req, res) => {
+    const id = parseInt(req.params.id);
 
-    const pokemon = [
-        "Bulbasaur",
-        "Ivysaur",
-        "Venusaur",
-        "Charmander",
-        "Charmeleon",
-        "Charizard",
-        "Squirtle",
-        "Wartortle",
-        "Blastoise",
-        "Caterpie",
-        "Metapod",
-        "Butterfree",
-        "Weedle",
-        "Kakuna",
-        "Beedrill",
-        "Pidgey",
-        "Pidgeotto",
-        "Pidgeot",
-        "Rattata",
-        "Raticate",
-        "Spearow",
-        "Fearow",
-        "Ekans",
-        "Arbok",
-        "Pikachu",
-        "Raichu",
-        "Sandshrew",
-        ]
-    let name = pokemon[id - 1];
-
-    res.send(`Hello, ${name}!`)
+    let pokemon = pokemons.find(pokemon => pokemon.id === id);
+    if (!pokemon || typeof id !== "number") {
+        res.status(404).json({error: "Le pokemon demandé n'existe pas"});
+        return;
+    }
+    res.status(200).json(pokemon);
 })
 app.listen(port, () => console.log(`app listening on port ${port}!`));
