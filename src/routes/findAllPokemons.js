3b2ac7149bb9538d159131be39abd1b62fce8b75
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-11-27 19:44:49 +0100

    Added advanced query and sorting of the data returned

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,16 +1,42 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require("sequelize")
 
 module.exports = (app) => {
     app.get('/api/pokemons', (req, res) => {
-        Pokemon.findAll()
-            .then(pokemons => {
-                let message = 'La liste des pokémons a bien été récupérée.'
-                if (pokemons.length >= 0) message = "La liste des pokémons est vide."
-                res.json({ message, data: pokemons })
+        if (req.query.name) {
+            const name = req.query.name;
+            const limit = parseInt(req.query.limit) || 5
+
+            if (name.length <= 1) {
+                const message = "Vous devez saisir au minimum 2 caractères pour que votre requête soit valide."
+                return res.status(401).json({message})
+            }
+
+            return Pokemon.findAndCountAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${name}%`,
+                    }
+                },
+                order: ['name'],
+                limit
             })
-            .catch((error) => {
-                const message = "La liste des pokémons n'a pas pu être recupérée."
-                res.status(500).json({message, error})
+            .then(({count, rows}) => {
+                const message = `Il y a ${count} qui correspondent au terme de recherche ${name}.`
+                return res.json({ message, data: rows })
             })
+        }
+        else {
+            Pokemon.findAll({order: ['name']})
+                .then(pokemons => {
+                    let message = 'La liste des pokémons a bien été récupérée.'
+                    if (pokemons.length <= 0) message = "La liste des pokémons est vide."
+                    res.json({ message, data: pokemons })
+                })
+                .catch((error) => {
+                    const message = "La liste des pokémons n'a pas pu être recupérée."
+                    res.status(500).json({message, error})
+                })
+        }
     })
 }
