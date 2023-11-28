b50681d57db687599947900487fee7ef0184404f
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-11-28 18:26:50 +0100

    fixed db related files for production

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
         dialectOptions: {
             timezone: 'Etc/GMT-2',
         },
-        logging: true
+        logging: false
     })
 }else {
     sequelize = new Sequelize('pokedex', 'john', process.env.DB_PASSWORD, {
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
         dialectOptions: {
             timezone: 'Etc/GMT-2',
         },
-        logging: true
+        logging: false
     })
 }
 
@@ -32,24 +32,7 @@ const Pokemon = PokemonModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
 
 const initDb = () => {
-    return sequelize.sync({force: true}).then(_ => {
-        pokemons.map(pokemon => {
-            Pokemon.create({
-                name: pokemon.name,
-                hp: pokemon.hp,
-                cp: pokemon.cp,
-                picture: pokemon.picture,
-                types: pokemon.types
-            })
-                .then(pokemon => console.log(pokemon.toJSON()))
-        })
-
-        bcrypt.hash('pikachu', 10)
-            .then(hash => User.create({ username: 'pikachu', password: hash }))
-            .then(user => console.log(user.toJSON()))
-
-        console.log('La base de donnée a bien été initialisée !')
-    })
+    return sequelize.sync()
 }
 
 module.exports = {
